07fe5307e6cc298cf7393f7d9a31be8ab4276d7d
伊欧 <langyo.china@gmail.com>
2023-05-22 01:08:38 +0800

    📝 Add basic demo for all wasm runtime.

--- crates/wasmer_runtime/src/main.rs
+++ crates/wasmer_runtime/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::{io::Read, path::Path, process::Command};
+
+use wasmer::{Module, Store};
+use wasmer_wasix::{Pipe, WasiEnv};
+
+fn main() -> Result<()> {
+    if !Path::new("target/wasm32-wasi/debug/wasi_hello_world.wasm").exists() {
+        let output = Command::new("cargo")
+            .args(&[
+                "build",
+                "--package",
+                "wasi_hello_world",
+                "--target",
+                "wasm32-wasi",
+            ])
+            .output()
+            .expect("failed to execute process");
+        println!("cargo build: {:?}", output);
+    }
+    let wasm_bytes = std::fs::read("target/wasm32-wasi/debug/wasi_hello_world.wasm")?;
+
+    let mut store = Store::default();
+    let module = Module::new(&store, wasm_bytes)?;
+    let (stdout_tx, mut stdout_rx) = Pipe::channel();
+
+    WasiEnv::builder("hello-world")
+        .stdout(Box::new(stdout_tx))
+        .run_with_store(module, &mut store)?;
+
+    let mut buf = String::new();
+    stdout_rx.read_to_string(&mut buf).unwrap();
+    println!("{}", buf);
+
+    Ok(())
 }
--- crates/wasmtime_runtime/src/main.rs
+++ crates/wasmtime_runtime/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::{path::Path, process::Command};
+
+use wasmtime::*;
+use wasmtime_wasi::sync::WasiCtxBuilder;
+
+fn main() -> Result<()> {
+    if !Path::new("target/wasm32-wasi/debug/wasi_hello_world.wasm").exists() {
+        let output = Command::new("cargo")
+            .args(&[
+                "build",
+                "--package",
+                "wasi_hello_world",
+                "--target",
+                "wasm32-wasi",
+            ])
+            .output()
+            .expect("failed to execute process");
+        println!("cargo build: {:?}", output);
+    }
+
+    let engine = Engine::default();
+    let mut linker = Linker::new(&engine);
+    wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
+
+    let wasi = WasiCtxBuilder::new()
+        .inherit_stdio()
+        .inherit_args()?
+        .build();
+    let mut store = Store::new(&engine, wasi);
+
+    let module = Module::from_file(&engine, "target/wasm32-wasi/debug/wasi_hello_world.wasm")?;
+    linker.module(&mut store, "", &module)?;
+    linker
+        .get_default(&mut store, "")?
+        .typed::<(), ()>(&store)?
+        .call(&mut store, ())?;
+
+    Ok(())
 }
